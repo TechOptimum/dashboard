ea32b9e8ac3f1fa354f956960a2ea7da474f4f27
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-05-25 20:54:27 -0500

    Themes

---
+++ styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    secondary: "#F9DBBD",
+    primary: "#0D0628",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        fontFamily: "Darker Grotesque, sans-serif",
+        color: "white",
+        backgroundColor: "secondary",
+      },
+      "&::-webkit-scrollbar": {
+        width: "0.5em",
+      },
+      "&::-webkit-scrollbar-track": {
+        borderRadius: "0px",
+        background: "transparent",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        background: "primary",
+        borderRadius: "50px",
+      },
+    }),
+  },
+});
+
+export default theme;

33062ca7504d0fd1f4c897c027b8e89b0524cc03
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-05-31 20:57:48 -0500

    Wahtecewr i have

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
-import CourseCard from "../comps/courseCard";
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, Text, VStack, Flex, Avatar } from "@chakra-ui/react";
+import { MdPersonOutline } from "react-icons/md";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Container maxW="100%" w="60%" className="courses">
-        <CourseCard />
-      </Container>
+      <Flex w="100%" justifyContent="center">
+        <VStack ml="200px" spacing="30px">
+          <Box mt="100px" width="400px" height="100px" className="welcome">
+            <Avatar
+              size="lg"
+              bg="white"
+              icon={<MdPersonOutline fontSize="2.5rem" />}
+              mt="20px"
+              ml="32px"
+            />
+            <Text className="user" fontSize="3xl" ml="40px">
+              {" "}
+              <span className="gradient userWelcome">Welcome</span> USER
+            </Text>
+          </Box>
+          <Box
+            className="welcome hackathon_background"
+            width="900px"
+            height="300px"
+            pos="relative"
+          >
+            <Image
+              height="300px"
+              width="900px"
+              src="/hackathon-background.png"
+              className="hackImage"
+            />
+            <Text
+              pos="absolute"
+              className="hackDesc"
+              fontSize="5xl"
+              fontWeight="semibold"
+              ml="20px"
+            >
+              Register for our <span className="gradient">Hackathon</span>
+            </Text>
+          </Box>
+        </VStack>
+      </Flex>
     </>
   );
 }
--- pages/workshops.js
+++ pages/workshops.js
@@ -1,9 +1,13 @@
 import React from "react";
+import CourseCard from "../comps/courseCard";
+import { Container } from "@chakra-ui/react";
 
 export default function workshops() {
   return (
-    <h1>
-      <div>Workshops</div>
-    </h1>
+    <>
+      <Container maxW="100%" w="60%" className="courses">
+        <CourseCard />
+      </Container>
+    </>
   );
 }

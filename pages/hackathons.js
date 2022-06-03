72e4fd3373474f999dae66c199ae5bdeb243ba37
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-06-02 23:05:48 -0500

    Fun

--- pages/hackathons.js
+++ pages/hackathons.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Box, Text, Center } from "@chakra-ui/react";
 
 function hackathons() {
   return (
-    <div>
-      <h1>hackathons</h1>
-    </div>
+    <Center ml="200px">
+      <Box
+        as="div"
+        width="400px"
+        height="200px"
+        bg="rgba(5, 0, 59, 0.5)"
+        position="relative"
+        mt="350px"
+      >
+        <Box>
+          <Text
+            position="absolute"
+            fontSize="8xl"
+            fontWeight="semibold"
+            ml="80px"
+            mt="20px"
+          >
+            P<span className="gradient">OO</span>P
+          </Text>
+        </Box>
+      </Box>
+    </Center>
   );
 }
 

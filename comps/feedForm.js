533b6e3b9579247327da7c8218dd2f28cd77bde2
PranTanTheMan <Seriousbro23@gmail.com>
2022-06-06 12:02:57 -0500

    Comp for form

---
+++ comps/feedForm.js
@@ -0,0 +1,91 @@
+import { Formik, Field } from "formik";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+
+export default function feedback_form() {
+  return (
+    <>
+      <Flex align="center" justify="center" ml="120px" h="100vh">
+        <Box bg="rgba(5, 0, 59, 0.439)" p={6} rounded="md" w={64}>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+              rememberMe: false,
+            }}
+            onSubmit={(values) => {
+              alert(JSON.stringify(values, null, 2));
+            }}
+          >
+            {({ handleSubmit, errors, touched }) => (
+              <form onSubmit={handleSubmit}>
+                <VStack spacing={4} align="flex-start">
+                  <FormControl>
+                    <FormLabel color="white" htmlFor="email">
+                      Email Address
+                    </FormLabel>
+                    <Field
+                      as={Input}
+                      id="email"
+                      name="email"
+                      type="email"
+                      variant="filled"
+                    />
+                  </FormControl>
+                  <FormControl
+                    isInvalid={!!errors.password && touched.password}
+                  >
+                    <FormLabel htmlFor="password" color="white">
+                      Password
+                    </FormLabel>
+                    <Field
+                      as={Input}
+                      id="password"
+                      name="password"
+                      type="password"
+                      variant="filled"
+                      color="white"
+                      validate={(value) => {
+                        let error;
+
+                        if (value.length < 5) {
+                          error = "Password must contain at least 6 characters";
+                        }
+
+                        return error;
+                      }}
+                    />
+                    <FormErrorMessage>{errors.password}</FormErrorMessage>
+                  </FormControl>
+                  <Field
+                    as={Checkbox}
+                    id="rememberMe"
+                    name="rememberMe"
+                    colorScheme="orange"
+                    color="white"
+                  >
+                    Remember me?
+                  </Field>
+                  <Button type="submit" colorScheme="orange" width="full">
+                    Login
+                  </Button>
+                </VStack>
+              </form>
+            )}
+          </Formik>
+        </Box>
+      </Flex>
+      ;
+    </>
+  );
+}

60f1e09201c3e037848a965888ffde736cccd41b
PranTanTheMan <Seriousbro23@gmail.com>
2022-06-06 20:43:09 -0500

    Updated Form

--- comps/feedForm.js
+++ comps/feedForm.js
@@ -1,4 +1,4 @@
-import { Formik, Field } from "formik";
+import { Formik, Field, Form } from "formik";
 import {
   Box,
   Button,
@@ -9,83 +9,134 @@ import {
   FormErrorMessage,
   Input,
   VStack,
+  Text,
+  Center,
 } from "@chakra-ui/react";
 import React from "react";
 
-export default function feedback_form() {
+function validateName(value) {
+  let error;
+  if (!value) {
+    error = "Name is required";
+  } else if (value.toLowerCase() !== "naruto") {
+    error = "Jeez! You're not a fan 😱";
+  }
+  return error;
+}
+
+function Feedform() {
   return (
     <>
-      <Flex align="center" justify="center" ml="120px" h="100vh">
-        <Box bg="rgba(5, 0, 59, 0.439)" p={6} rounded="md" w={64}>
+      <Flex w="100%" justify="center" ml="200px">
+        <VStack mb="100px">
           <Formik
-            initialValues={{
-              email: "",
-              password: "",
-              rememberMe: false,
-            }}
-            onSubmit={(values) => {
-              alert(JSON.stringify(values, null, 2));
+            initialValues={{ name: "Sasuke" }}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                actions.setSubmitting(false);
+              }, 1000);
             }}
           >
-            {({ handleSubmit, errors, touched }) => (
-              <form onSubmit={handleSubmit}>
-                <VStack spacing={4} align="flex-start">
-                  <FormControl>
-                    <FormLabel color="white" htmlFor="email">
-                      Email Address
-                    </FormLabel>
-                    <Field
-                      as={Input}
-                      id="email"
-                      name="email"
-                      type="email"
-                      variant="filled"
-                    />
-                  </FormControl>
-                  <FormControl
-                    isInvalid={!!errors.password && touched.password}
-                  >
-                    <FormLabel htmlFor="password" color="white">
-                      Password
-                    </FormLabel>
-                    <Field
-                      as={Input}
-                      id="password"
-                      name="password"
-                      type="password"
-                      variant="filled"
-                      color="white"
-                      validate={(value) => {
-                        let error;
-
-                        if (value.length < 5) {
-                          error = "Password must contain at least 6 characters";
-                        }
-
-                        return error;
-                      }}
-                    />
-                    <FormErrorMessage>{errors.password}</FormErrorMessage>
-                  </FormControl>
-                  <Field
-                    as={Checkbox}
-                    id="rememberMe"
-                    name="rememberMe"
-                    colorScheme="orange"
-                    color="white"
-                  >
-                    Remember me?
-                  </Field>
-                  <Button type="submit" colorScheme="orange" width="full">
-                    Login
-                  </Button>
-                </VStack>
-              </form>
+            {(props) => (
+              <Form>
+                <Field name="name" validate={validateName}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                    >
+                      <FormLabel htmlFor="name">First name</FormLabel>
+                      <Input {...field} id="name" placeholder="name" />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button
+                  mt={4}
+                  colorScheme="teal"
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Form>
             )}
           </Formik>
-        </Box>
+        </VStack>
       </Flex>
-      ;
     </>
   );
 }
+export default Feedform;
+
+// <Flex align="center" justify="center" ml="120px" h="100vh">
+//         <Box bg="rgba(5, 0, 59, 0.439)" p={6} rounded="md" w={64}>
+//           <Formik
+//             initialValues={{
+//               email: "",
+//               password: "",
+//               rememberMe: false,
+//             }}
+//             onSubmit={(values) => {
+//               alert(JSON.stringify(values, null, 2));
+//             }}
+//           >
+//             {({ handleSubmit, errors, touched }) => (
+//               <form onSubmit={handleSubmit}>
+//                 <VStack spacing={4} align="flex-start">
+//                   <FormControl>
+//                     <FormLabel color="white" htmlFor="email">
+//                       Email Address
+//                     </FormLabel>
+//                     <Field
+//                       as={Input}
+//                       id="email"
+//                       name="email"
+//                       type="email"
+//                       variant="filled"
+//                     />
+//                   </FormControl>
+//                   <FormControl
+//                     isInvalid={!!errors.password && touched.password}
+//                   >
+//                     <FormLabel htmlFor="password" color="white">
+//                       Password
+//                     </FormLabel>
+//                     <Field
+//                       as={Input}
+//                       id="password"
+//                       name="password"
+//                       type="password"
+//                       variant="filled"
+//                       color="white"
+//                       validate={(value) => {
+//                         let error;
+
+//                         if (value.length < 5) {
+//                           error = "Password must contain at least 6 characters";
+//                         }
+
+//                         return error;
+//                       }}
+//                     />
+//                     <FormErrorMessage>{errors.password}</FormErrorMessage>
+//                   </FormControl>
+//                   <Field
+//                     as={Checkbox}
+//                     id="rememberMe"
+//                     name="rememberMe"
+//                     colorScheme="orange"
+//                     color="white"
+//                   >
+//                     Remember me?
+//                   </Field>
+//                   <Button type="submit" colorScheme="orange" width="full">
+//                     Login
+//                   </Button>
+//                 </VStack>
+//               </form>
+//             )}
+//           </Formik>
+//         </Box>
+//       </Flex>
+//       ;

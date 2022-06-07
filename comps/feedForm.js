ed04105b6d25f03cbeb00307adeff743db9f49a1
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-06-06 22:58:36 -0500

    Form is 99% done

--- comps/feedForm.js
+++ comps/feedForm.js
@@ -7,19 +7,22 @@ import {
   FormControl,
   FormLabel,
   FormErrorMessage,
-  Input,
   VStack,
   Text,
-  Center,
+  Textarea,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
 } from "@chakra-ui/react";
 import React from "react";
+import TextareaAutosize from "react-textarea-autosize";
 
 function validateName(value) {
   let error;
   if (!value) {
     error = "Name is required";
-  } else if (value.toLowerCase() !== "naruto") {
-    error = "Jeez! You're not a fan 😱";
   }
   return error;
 }
@@ -27,116 +30,159 @@ function validateName(value) {
 function Feedform() {
   return (
     <>
-      <Flex w="100%" justify="center" ml="200px">
-        <VStack mb="100px">
-          <Formik
-            initialValues={{ name: "Sasuke" }}
-            onSubmit={(values, actions) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                actions.setSubmitting(false);
-              }, 1000);
-            }}
-          >
-            {(props) => (
-              <Form>
-                <Field name="name" validate={validateName}>
-                  {({ field, form }) => (
-                    <FormControl
-                      isInvalid={form.errors.name && form.touched.name}
-                    >
-                      <FormLabel htmlFor="name">First name</FormLabel>
-                      <Input {...field} id="name" placeholder="name" />
-                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
-                <Button
-                  mt={4}
-                  colorScheme="teal"
-                  isLoading={props.isSubmitting}
-                  type="submit"
-                >
-                  Submit
-                </Button>
-              </Form>
-            )}
-          </Formik>
+      <Flex w="100%" justify="center">
+        <VStack mt="100px" mb="100px" ml="220px" spacing="20px">
+          <Box width="700px" height="160px" className="formBox">
+            <Text align="center" fontSize="3xl" mt="10px" className="gradient">
+              Feedback Form
+            </Text>
+            <Text align="left" mx="20px" mt="15px">
+              Using this Feedback form, you can respond to the questions below
+              regarding Tech Optimum's staff. Responses are anonymous so please
+              feel free with your responses. Constructive feedback is very much
+              appreciated, Please be respectful
+            </Text>
+          </Box>
+          <Box className="formBox" width="700px">
+            <Formik initialValues={{ name: "" }}>
+              {(props) => (
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}
+                        isRequired
+                        mx="20px"
+                        my="20px"
+                      >
+                        <FormLabel htmlFor="name">
+                          On a scale of 1-10, How good is the User Interface of
+                          the Dashboard
+                        </FormLabel>
+                        <NumberInput
+                          defaultValue="0"
+                          min={0}
+                          max={10}
+                          keepWithinRange={true}
+                          clampValueOnBlur={true}
+                          width="650px"
+                        >
+                          <NumberInputField />
+                          <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                          </NumberInputStepper>
+                        </NumberInput>
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+          <Box className="formBox" width="700px">
+            <Formik initialValues={{ name: "" }}>
+              {(props) => (
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}
+                        isRequired
+                        mx="20px"
+                        my="20px"
+                      >
+                        <FormLabel htmlFor="name">
+                          Are their any bugs you're experiencing with the
+                          dashboard?
+                        </FormLabel>
+                        <Textarea
+                          {...field}
+                          minH="fit-content"
+                          placeholder="respond here"
+                          resize="none"
+                          as={TextareaAutosize}
+                          width="650px"
+                        />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+          <Box className="formBox" width="700px">
+            <Formik initialValues={{ name: "" }}>
+              {(props) => (
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}
+                        mx="20px"
+                        my="20px"
+                      >
+                        <FormLabel htmlFor="name">
+                          What is one feature that you would like to see added
+                          to the dashboard?
+                        </FormLabel>
+                        <Textarea
+                          {...field}
+                          minH="fit-content"
+                          placeholder="respond here"
+                          resize="none"
+                          as={TextareaAutosize}
+                          width="650px"
+                        />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+          <Box className="formBox" width="700px">
+            <Formik initialValues={{ name: "" }}>
+              {(props) => (
+                <Form>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}
+                        mx="20px"
+                        my="20px"
+                      >
+                        <FormLabel htmlFor="name">
+                          Where did you hear about Tech Optimum?
+                        </FormLabel>
+                        <VStack spacing="5px" float="left">
+                          <Checkbox>
+                            Through the Tech Optimum's Website
+                          </Checkbox>
+                          <Checkbox>
+                            Through the Tech Optimum's Website
+                          </Checkbox>
+                          <Checkbox>
+                            Through the Tech Optimum's Website
+                          </Checkbox>
+                          <Checkbox>
+                            Through the Tech Optimum's Website
+                          </Checkbox>
+                        </VStack>
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </Form>
+              )}
+            </Formik>
+          </Box>
         </VStack>
       </Flex>
     </>
   );
 }
 export default Feedform;
-
-// <Flex align="center" justify="center" ml="120px" h="100vh">
-//         <Box bg="rgba(5, 0, 59, 0.439)" p={6} rounded="md" w={64}>
-//           <Formik
-//             initialValues={{
-//               email: "",
-//               password: "",
-//               rememberMe: false,
-//             }}
-//             onSubmit={(values) => {
-//               alert(JSON.stringify(values, null, 2));
-//             }}
-//           >
-//             {({ handleSubmit, errors, touched }) => (
-//               <form onSubmit={handleSubmit}>
-//                 <VStack spacing={4} align="flex-start">
-//                   <FormControl>
-//                     <FormLabel color="white" htmlFor="email">
-//                       Email Address
-//                     </FormLabel>
-//                     <Field
-//                       as={Input}
-//                       id="email"
-//                       name="email"
-//                       type="email"
-//                       variant="filled"
-//                     />
-//                   </FormControl>
-//                   <FormControl
-//                     isInvalid={!!errors.password && touched.password}
-//                   >
-//                     <FormLabel htmlFor="password" color="white">
-//                       Password
-//                     </FormLabel>
-//                     <Field
-//                       as={Input}
-//                       id="password"
-//                       name="password"
-//                       type="password"
-//                       variant="filled"
-//                       color="white"
-//                       validate={(value) => {
-//                         let error;
-
-//                         if (value.length < 5) {
-//                           error = "Password must contain at least 6 characters";
-//                         }
-
-//                         return error;
-//                       }}
-//                     />
-//                     <FormErrorMessage>{errors.password}</FormErrorMessage>
-//                   </FormControl>
-//                   <Field
-//                     as={Checkbox}
-//                     id="rememberMe"
-//                     name="rememberMe"
-//                     colorScheme="orange"
-//                     color="white"
-//                   >
-//                     Remember me?
-//                   </Field>
-//                   <Button type="submit" colorScheme="orange" width="full">
-//                     Login
-//                   </Button>
-//                 </VStack>
-//               </form>
-//             )}
-//           </Formik>
-//         </Box>
-//       </Flex>
-//       ;
--- pages/feedback.js
+++ pages/feedback.js
@@ -3,7 +3,6 @@ import Feedform from "../comps/feedForm";
 export default function feedback() {
   return (
     <>
-      <h1>Hello</h1>
       <Feedform />
     </>
   );

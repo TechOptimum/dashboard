5c4d96247a129a9b62bd8ac6537b292dd750b682
Rohan Ramakrishnan <rohanramakrish@gmail.com>
2022-09-03 17:17:57 -0700

    add course page

---
+++ comps/coursepage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NotionRenderer } from "react-notion";
+import 'primsjs/themes/prism-tomorrow.css'
+
+function coursepage() {
+  return <div>coursepage</div>;
+}
+
+export default coursepage;
